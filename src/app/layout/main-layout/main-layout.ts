2fd1ac0b17879f8bb2c0bc6c56ba58de51984740
rowel <razonabler23@gmail.com>
2026-09-09 21:51:26 +0800

    feat: update layout header

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 type CurrentSidenav = 'home' | 'tasks' | 'settings';
 
@@ -16,11 +17,33 @@ type CurrentSidenav = 'home' | 'tasks' | 'settings';
     MatIconModule,
     RouterLink,
     MatToolbarModule,
+    MatButtonModule,
   ],
   templateUrl: './main-layout.html',
   styles: ``,
 })
 export class MainLayout {
-  openSideNav = false;
-  currentSideNav: CurrentSidenav = 'home';
+  openSideNav = signal<boolean>(false);
+  currentSideNav = signal<CurrentSidenav>('home');
+
+  title = computed(() => {
+    switch (this.currentSideNav()) {
+      case 'home':
+        return 'Good morning!';
+      case 'tasks':
+        return 'Tasks';
+      case 'settings':
+        return 'Settings';
+    }
+  });
+  description = computed(() => {
+    switch (this.currentSideNav()) {
+      case 'home':
+        return "Here's your task overview for today.";
+      case 'tasks':
+        return 'Manage your todos and stay organized.';
+      case 'settings':
+        return 'Settings Description later.';
+    }
+  });
 }
