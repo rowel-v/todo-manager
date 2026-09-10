8e27af3f6ab3dd929917ecbbb75d84612e23b2ae
rowel <razonabler23@gmail.com>
2026-09-10 21:40:51 +0800

    feat: add Lucide icons and implement own sidebar

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,35 +1,36 @@
 import { Component, computed, signal } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import {
+  LucideMenu,
+  LucideHouse,
+  LucideBadgeCheck,
+  LucideSettings,
+  LucideX,
+} from '@lucide/angular';
 
 type CurrentSidenav = 'home' | 'tasks' | 'settings';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
-    MatSidenavModule,
-    MatListModule,
     RouterOutlet,
-    MatIconModule,
     RouterLink,
-    MatToolbarModule,
-    MatButtonModule,
+    LucideMenu,
+    LucideHouse,
+    LucideBadgeCheck,
+    LucideSettings,
+    LucideX,
   ],
   templateUrl: './main-layout.html',
   styles: ``,
 })
 export class MainLayout {
-  openSideNav = signal<boolean>(false);
+  openSideNav = signal<boolean>(true);
   currentSideNav = signal<CurrentSidenav>('home');
-
   title = computed(() => {
     switch (this.currentSideNav()) {
       case 'home':
-        return 'Good morning!';
+        return 'Good morning';
       case 'tasks':
         return 'Tasks';
       case 'settings':
